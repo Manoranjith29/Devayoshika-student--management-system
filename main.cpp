7b559458c3625df6863767ba80b85744b1975e25
Manoranjith29 <ranjithmano386@gmail.com>
2026-08-18 22:54:25 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "==============================" << endl;
+    cout << "   STUDENT MANAGEMENT SYSTEM" << endl;
+    cout << "==============================" << endl;
+
+    return 0;
+}
